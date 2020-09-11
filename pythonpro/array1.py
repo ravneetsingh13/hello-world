01f8ee4e2b3b9b17b9c4f34fcad7171f5b851463
Ravneet Singh <ravneetsingh13@live.com>
2020-09-12 03:18:00 +0530

    deleted pythonpro

--- pythonpro/array1.py
+++
@@ -1,9 +0,0 @@
-import array as arr
-a = arr.array('i',[1,2,3,4,5])
-b = arr.array(a.typecode, [i*i for i in a])
-b.reverse()
-for e in a:
-	print(e,end=' ')
-print('\n')
-for e in b:
-	print(e,end=' ')
